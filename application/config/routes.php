b859e1f3f850e546249dc1c4b4dbf0ac24204a92
Johan Spann <Spann@Wades-MacBook.local>
2014-01-15 20:27:19 +0100

    Removed the index.php file from the url

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,8 +38,9 @@
 |
 */
 
-$route['default_controller'] = "welcome";
+$route['default_controller'] = 'welcome';
 $route['404_override'] = '';
+$route['gallery'] = 'gallery';
 
 
 /* End of file routes.php */
