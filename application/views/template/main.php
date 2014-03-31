b5441235fcff31ca228e0cc97beb6e0b74f9c495
Balzzanar <Balzzanar@users.noreply.github.com>
2014-03-31 16:44:06 +0200

    Removed unneeded code

--- application/views/template/main.php
+++ application/views/template/main.php
@@ -16,10 +16,4 @@
 
 	<?php echo $main_content; ?>
 
-
-	<!-- Bootstrap! -->
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="public/bootstrap/js/bootstrap.min.js"></script>
 </body>
