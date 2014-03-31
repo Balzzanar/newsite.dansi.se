8dbb588e934b2658688cf16ef10a5f8f7f6710a4
Balzzanar <Balzzanar@users.noreply.github.com>
2014-03-31 16:36:26 +0200

    Fixed a broken link

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -3,7 +3,7 @@
 <!-- Load Bootstrap core JavaScript -->
 <?php # <script src="http://code.jquery.com/jquery.js"></script> ?>
 <script language="javascript" type="text/javascript" src="public/js/jquery-2.0.3.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+<script src="public/bootstrap/js/bootstrap.min.js"></script>
 <script src="public/bootstrap/js/holder.js"></script>
 </body>
 </html>
