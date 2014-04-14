31e39ae9dab02100c55310c957bf63ed41fcf343
Wades <spann.johan@gmail.com>
2014-04-14 22:04:12 +0200

    Removed unwanted char

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -3,7 +3,7 @@
 <!-- Load Bootstrap core JavaScript -->
 <script src="http://code.jquery.com/jquery.js"></script>
 <script src="<?php echo base_url().'public/bootstrap/js/bootstrap.min.js'; ?>"></script>
-<script src="<?php echo base_url().'public/bootstrap/js/holder.js'; ?>"></script>i
+<script src="<?php echo base_url().'public/bootstrap/js/holder.js'; ?>"></script>
 <script src="<?php echo base_url().'public/js/scripts.js'; ?>"></script>
 
 </body>
